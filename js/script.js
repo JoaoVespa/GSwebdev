5d706a5a1f2f0391e284d701fa2b7476cf9b419a
Joao Vespa <joaom7836@gmail.com>
2025-06-05 19:20:03 -0300

    quiz parte JS

--- js/script.js
+++ js/script.js
@@ -38,7 +38,41 @@ document.getElementById('cliente-form').addEventListener('submit', function (e) 
   mensagem.style.color = 'green';
 });
 
+//quiz
+document.getElementById('quiz-form').addEventListener('submit', function(e) {
+  e.preventDefault();
 
+  const respostasCorretas = {
+    q1: 'b',
+    q2: 'c',
+    q3: 'b',
+    q4: 'b',
+    q5: 'b',
+    q6: 'b',
+    q7: 'b',
+    q8: 'c',
+    q9: 'b',
+    q10: 'b'
+  };
+
+  let pontuacao = 0;
+
+  for (let key in respostasCorretas) {
+    const respostaUsuario = document.querySelector(`input[name="${key}"]:checked`);
+    if (respostaUsuario && respostaUsuario.value === respostasCorretas[key]) {
+      pontuacao++;
+    }
+  }
+
+  const resultado = document.getElementById('quiz-result');
+  resultado.textContent = `Você acertou ${pontuacao} de 10 perguntas.`;
+
+  if (pontuacao < 6) {
+    resultado.style.color = 'red';
+  } else {
+    resultado.style.color = 'green';
+  }
+});
 
 
 
